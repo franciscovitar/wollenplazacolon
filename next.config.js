e155a70845af85852fbf80d7a5c47ee2db2402dd
franciscovitar <Franvitar15@gmail.com>
2026-01-25 22:53:06 -0300

    Fix: normalizar nombres de imágenes de galería a minúsculas y agregar image qualities config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [25, 50, 75, 100],
     minimumCacheTTL: 60 * 60 * 24 * 365, // 1 año
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   compress: true,
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ['framer-motion', 'react-icons'],
